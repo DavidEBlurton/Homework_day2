f2b69760be97f38fbcc1316164a33a74e9d57f16
DavidEBlurton <163071893+DavidEBlurton@users.noreply.github.com>
2024-07-18 14:58:58 -0700

    first commit

---
+++ Adventure_game.py
@@ -0,0 +1,57 @@
+# orginal code below before corrections------------------------------------------------------
+
+# place = input("Choose a place: forest or cave? ")
+# if place = "forest":
+#    action = input("climb a tree or cross a river?")
+#    if action = "climb a tree":
+#        print("You found a bird's nest!")
+#    else action = "cross a river":
+#        print("You found a boat!")
+# elif place = "cave":
+#   print("You find a hidden treasure!")
+
+# code correction below-------------------------------------------
+
+
+# if Place == "forest":
+#  action = input("climb a tree or cross a river?")
+#    if action == "climb a tree":
+#        print("You found a bird's nest!")
+#    else : action = "cross a river"
+#   print("You found a boat!")
+# elif Place == "cave":
+#    print("You find a hidden treasure!")
+
+
+# Task 2 expanding the story ================================================
+
+
+# Place = input("Choose a place: forest or cave? ")
+
+# if Place == "forest":
+    # action = input("climb a tree or cross a river?")
+    #   print("You found a bird's nest!")
+    # else : action = "cross a river"
+    # print("You found a boat!")
+# elif Place == "cave":
+    # print("light a torch" or "proceed in the dark")
+    # action = input("light a torch or proceed in the dark?")
+    # if action == "light a torch":
+        # print("you now see that there is a rope bridge to your left")
+    # else : action = "proceed in the dark"
+    # print("you   off a cliff!")
+
+    # Task 3 =================================================================
+
+ # Place == "forest":
+    # action = input("climb a tree or cross a river?")
+    #   print("You found a bird's nest!")
+    # else : action = "cross a river"
+    # print("You found a boat!")
+# elif Place == "cave":
+    # print("light a torch" or "proceed in the dark")
+    # action = input("light a torch or proceed in the dark?")
+    # if action == "light a torch":
+        # print("you now see that there is a rope bridge to your left")
+    # else : action = "proceed in the dark"
+    # print("you   off a cliff!")
---
+++ Event_planner.py
@@ -0,0 +1,10 @@
+# task 1 code correction 
+
+# attendees = input("Enter number of attendees: ")
+# venue = "large hall" if attendees > '100' else "conference room"
+# print(venue)
+
+# Task 2 Catering Chocies
+
+food = input("Would you like vegetarian food?")
+print('You should try Veggie Delight Caterers!' if food == 'yes' else 'Then you should go to Gourmet Meals Caterers!')
